6f5298f9a530ad88bad561c77c0c5a6386a26de3
Vaclav Bohac <bohac.v@gmail.com>
2015-12-15 12:01:48 +0100

    Move functions to own files

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-import Error from './lib/error';
-import { isFunction, validate } from './lib/utils';
+import partial from './lib/partial';
+import compose from './lib/compose';
+import curry from './lib/curry';
 
-export const compose = (...callbacks) => {
-  const composite = (...args) => {
-    const initialValue = callbacks[callbacks.length - 1](...args);
-    return callbacks.reduceRight((previousResult, callback) => {
-      return callback(previousResult);
-    }, initialValue);
-  };
-
-  const isValid = (callbacks.length !== 0 && validate(callbacks, isFunction));
-
-  if (!isValid) {
-    throw Error('Supplied arguments are not functions.');
-  }
-
-  return composite;
-};
-
-export const curry = (fn) => {
-  const createCurry = (...boundArgs) => {
-    return (...currentArgs) => {
-      const args = boundArgs.concat(currentArgs);
-      return args.length < fn.length ? createCurry(...args) : fn(...args);
-    };
-  };
-
-  if (!isFunction(fn)) {
-    throw Error('Supplied argument is not a function.');
-  }
-
-  return createCurry();
-};
-
-export const partial = (fn, ...boundArgs) => {
-  const boundFunction = (...args) => {
-    return fn(...boundArgs.concat(args));
-  };
-
-  if (!isFunction(fn)) {
-    throw Error('First argument is not a function.');
-  }
-
-  return boundFunction;
-};
+export { partial, compose, curry };
